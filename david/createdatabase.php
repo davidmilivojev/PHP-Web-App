93e2647d3aab30dde8ac71a43032131355ba2447
David Milivojev <davyd7_@hotmail.com>
2017-12-18 02:00:19 +0100

    Update createdatabase.php

--- david/createdatabase.php
+++ david/createdatabase.php
@@ -20,13 +20,13 @@ $kreirajTabeluKonferencija = "CREATE TABLE IF NOT EXISTS `konferencija` (
 
 $kreirajTabeluPredavanja = "CREATE TABLE IF NOT EXISTS `predavanje` (
   `idPredavanje` bigint(20) NOT NULL AUTO_INCREMENT,
-  `Naziv` varchar(250) COLLATE NOT NULL,
-  `Predavaci` varchar(250) COLLATE NOT NULL,
+  `Naziv` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `Predavaci` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
   `Pocetak` datetime NOT NULL,
   `Kraj` datetime NOT NULL,
-  `idKonferencija` bigint(20) NOT NULL,
+  `Konferencija` bigint(20) NOT NULL,
   PRIMARY KEY (`idPredavanje`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1";
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;";
 
 
 $kreirajTabeluKornisnik = "CREATE TABLE IF NOT EXISTS `korisnik` (
@@ -38,7 +38,7 @@ $kreirajTabeluKornisnik = "CREATE TABLE IF NOT EXISTS `korisnik` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;";
 
 
-$popuniTabeluRang = "INSERT INTO `rank` (`idRang`, `nazivRang`) VALUES
+$popuniTabeluRang = "INSERT INTO `rang` (`idRang`, `nazivRang`) VALUES
 (1, 'Medjunarodna'),
 (2, 'IEEE'),
 (3, 'Domaca');";
@@ -48,14 +48,21 @@ $popuniTabeluKonferencija = "INSERT INTO `konferencija` (`idKonferencija`, `Nazi
 (2, 'ITRO', 'Information Technology and Education Developement', 1),
 (3, 'IEEE SMC', 'Systems, Man, Cybernetics...', 2);";
 
+$popuniTabeluPredavanja = "INSERT INTO `predavanje` (`idPredavanje`, `Naziv`, `Predavaci`, `Pocetak`, `Kraj`, `Konferencija`) VALUES
+(1, 'prvo', 'it predavanje', '2017-12-26 03:04:03', '2017-12-28 03:06:09', 2),
+(2, 'drugo', 'it predavanje 2', '2017-11-28 17:06:00', '2017-12-27 00:00:00', 1);";
+
+
 
 mysql_query($kreirajBazu);
 mysql_select_db('konferencije_ias', $con);
 mysql_query($kreirajTabeluRang);
 mysql_query($kreirajTabeluKonferencija);
 mysql_query($kreirajTabeluKornisnik);
+mysql_query($kreirajTabeluPredavanja);
 mysql_query($popuniTabeluRang);
 mysql_query($popuniTabeluKonferencija);
+mysql_query($popuniTabeluPredavanja);
 
 echo "Uspesno kreirano ako nema upozorenja!!";
 
